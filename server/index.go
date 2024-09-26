a7b87be439576a0a9c13e0277cb4f52ad9740b8c
Sam Patterson <pattersonrsam@gmail.com>
2024-09-25 20:12:49 -0400

    more boiler

--- server/index.go
+++ server/index.go
@@ -1,10 +1,54 @@
-package main;
+package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
+var db *sql.DB
+
 func main() {
-	fmt.Println("Hello, World!")
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	db = startDbConnection()
+
+	http.HandleFunc("/", handleRoot)
+	http.HandleFunc("/hello", handleHello)
+
+	fmt.Println("Server starting on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func startDbConnection() *sql.DB {
+	connStr := os.Getenv("CONNECTION_STRING")
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatalf("Error opening database connection: %v", err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("Error pinging database: %v", err)
+	}
+
+	return db
+}
+
+func handleRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the Go backend!")
+}
+
+func handleHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
 }
