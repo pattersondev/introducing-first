7b88e11627c63dc1448943b73919ca0a9714723d
jackmcameron <jack.cameron@cox.net>
2024-10-01 11:47:24 -0400

    Working Connection and Insert DB calls

--- server/db/dbtoolbox.go
+++ server/db/dbtoolbox.go
@@ -17,7 +17,7 @@ func StartDbConnection() *sql.DB {
 	connStr := os.Getenv("CONNECTION_STRING")
 
 	var err error
-	db, err := sql.Open("postgres", connStr)
+	db, err = sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatalf("Error opening database connection: %v", err)
 	}
@@ -31,13 +31,13 @@ func StartDbConnection() *sql.DB {
 }
 
 func InsertFighter(fighter models.Fighter) error {
-	sqlStatement := "INSERT INTO public.fighter(name, nickname, division_title, status, hometown, octagon_debut, image_link, girth, stance, fighter_id, fighting_style, gym, age, height, weight, reach, leg_reach) VALUES ($10, $2, $3, $4, $5, $6, $7, $8, $9, $11, $12, $13, $14, $15, $16, $17);"
+	sqlStatement := "INSERT INTO public.fighter(name, nickname, division_title, status, hometown, octagon_debut, image_link, girth, stance, fighter_id, fighting_style, gym, age, height, weight, reach, leg_reach) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17);"
 
 	_, err := db.ExecContext(context.Background(), sqlStatement,
-		fighter.FighterID, fighter.Name, fighter.Nickname, fighter.DivisionTitle, fighter.Status,
+		fighter.Name, fighter.Nickname, fighter.DivisionTitle, fighter.Status,
 		fighter.Hometown, fighter.OctagonDebut, fighter.ImageLink, fighter.Girth, fighter.Stance,
-		fighter.FightingStyle, fighter.Gym, fighter.Age, fighter.Height, fighter.Weight,
-		fighter.Reach, fighter.LegReach)
+		fighter.FighterID, fighter.FightingStyle, fighter.Gym, fighter.Age, fighter.Height,
+		fighter.Weight, fighter.Reach, fighter.LegReach)
 
 	if err != nil {
 		return fmt.Errorf("unable to insert fighter: %w", err)
