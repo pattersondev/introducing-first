a0f18a7e360a2b1aa29327433628ea77f642d6a3
jackmcameron <jack.cameron@cox.net>
2024-10-02 17:03:51 -0400

    added SelectFighter Function (get fighter)

--- server/db/dbtoolbox.go
+++ server/db/dbtoolbox.go
@@ -31,6 +31,7 @@ func StartDbConnection() *sql.DB {
 }
 
 func InsertFighter(fighter models.Fighter) error {
+	//func to insert fighter into database. (used to create a new fighter in the DB)
 	sqlStatement := "INSERT INTO public.fighter(name, nickname, division_title, status, hometown, octagon_debut, image_link, girth, stance, fighter_id, fighting_style, gym, age, height, weight, reach, leg_reach) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17);"
 
 	_, err := db.ExecContext(context.Background(), sqlStatement,
@@ -46,6 +47,8 @@ func InsertFighter(fighter models.Fighter) error {
 }
 
 func BulkInsertFighters(fighters []models.Fighter) error {
+	//func to insert multiple fighters
+
 	//if fighters array is empty, don't do anything
 	if len(fighters) == 0 {
 		return nil
@@ -53,7 +56,6 @@ func BulkInsertFighters(fighters []models.Fighter) error {
 
 	//calling the existing InsertFighter function for each fighter in the fighter array
 	for _, fighter := range fighters {
-
 		if err := InsertFighter(fighter); err != nil {
 			return err
 		}
@@ -62,9 +64,30 @@ func BulkInsertFighters(fighters []models.Fighter) error {
 	return nil
 }
 
-// func getFighter(){
-// 	sqlStatement := ""
+func SelectFighter(fId int) (models.Fighter, error) {
+	//func to select a fighter from the database, based on the fighter's fighter_id (Primary Key)
+	//returns a fighter object created using data retrieved from DB (or an error)
 
-// 	var fighter models.Fighter
+	var fighter models.Fighter
 
-// }
+	sqlStatement := "SELECT name, nickname, division_title, status, hometown, octagon_debut, image_link, girth, stance, fighter_id, fighting_style, gym, age, height, weight, reach, leg_reach FROM public.fighter WHERE fighter_id = $1;"
+
+	row := db.QueryRowContext(context.Background(), sqlStatement, fId)
+
+	err := row.Scan(&fighter.Name, &fighter.Nickname, &fighter.DivisionTitle, &fighter.Status,
+		&fighter.Hometown, &fighter.OctagonDebut, &fighter.ImageLink, &fighter.Girth,
+		&fighter.Stance, &fighter.FighterID, &fighter.FightingStyle, &fighter.Gym,
+		&fighter.Age, &fighter.Height, &fighter.Weight, &fighter.Reach, &fighter.LegReach)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			//error message if no fighter is found for provided fighter id
+			return fighter, fmt.Errorf("No Fighter found with given fighter id of: ", fId)
+		}
+		//other message for other problems
+		return fighter, fmt.Errorf("Error when selecting fighter: ", err)
+	}
+
+	return fighter, nil
+
+}
