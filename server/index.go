6b2c1aa413c8a863c8833225af9d5ffd73e4fdff
jackmcameron <jack.cameron@cox.net>
2024-10-03 12:31:59 -0400

    WIP addEvent func

--- server/index.go
+++ server/index.go
@@ -109,6 +109,7 @@ func main() {
 	http.HandleFunc("/", handleRoot)
 	http.HandleFunc("/hello", handleHello)
 	http.HandleFunc("/fighter/create", addFighter)
+	http.HandleFunc("/event/create", addEvent)
 
 	fmt.Println("Server starting on :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
@@ -141,8 +142,31 @@ func addFighter(w http.ResponseWriter, r *http.Request) {
 	}
 
 	//print fighter for now, will eventually add the fighter to the db
-	fmt.Fprintf(w, "Recieved new fighter", newFighter)
+	fmt.Fprintf(w, "Recieved new fighter: ", newFighter)
 
 	//need to modify fighter class and validate schema of JSON before actually adding fighter. DB schema may change too
 	//db.InsertFighter(newFighter)
+}
+
+func addEvent(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Gimme dat event data")
+
+	//only allow processing if it's a Post Request
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var newEvent JSONEvent
+
+	//decoding json and creating temp event object / handling bad requests
+	err := json.NewDecoder(r.Body).Decode(&newEvent)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	//print event for now, will eventually add the event to the db
+	fmt.Fprintf(w, "Recieved new event: ", newEvent)
+
 }
